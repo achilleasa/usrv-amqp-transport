bc07da9e75ca070e53f718af55c7ba91a6e6cec2
Achilleas Anagnostopoulos <achilleas@taxibeat.com>
2015-11-17 23:02:43 +0000

    Define transport messages

---
+++ message.go
@@ -0,0 +1,36 @@
+package amqp
+
+import "github.com/achilleasa/usrv"
+
+// The internal message type used by the amqp transport.
+type amqpMessage struct {
+	from          string
+	to            string
+	property      usrv.Property
+	content       []byte
+	err           error
+	correlationId string
+
+	isReply   bool
+	replyTo   string
+	replyChan chan usrv.Message
+}
+
+func (m *amqpMessage) From() string {
+	return m.from
+}
+func (m *amqpMessage) To() string {
+	return m.to
+}
+func (m *amqpMessage) Property() usrv.Property {
+	return m.property
+}
+func (m *amqpMessage) CorrelationId() string {
+	return m.correlationId
+}
+func (m *amqpMessage) Content() ([]byte, error) {
+	return m.content, m.err
+}
+func (m *amqpMessage) SetContent(content []byte, err error) {
+	m.content, m.err = content, err
+}
